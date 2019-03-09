755956e1232b55f015552b423d9ea9f9e6869d38
allyjfuller <allyjfuller@gmail.com>
2019-03-09 02:23:48 -0500

    first commit

---
+++ src/Welcome.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
+
+export default Welcome;
